4e5dee0cd08db4a903517ca8da71ac3c13184c2c
inc <p.perez34000@laposte.net>
2024-04-21 20:39:49 +0200

    intéractivité de l'icone du menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,27 @@
 import logo from "../assets/logoalt.png";
-import menu from '../assets/list.svg'
+import menu from '../assets/list.svg';
+import cross from '../assets/close.svg';
 import { useScreenWidth } from "../hooks/useScreenWidth";
+import { useState } from "react";
 
-function Nav({ event }) {
+function Nav() {
+    //State pour switch l'icone du menu 
+    const [image, setImage] = useState(menu);
+    // hook personnalisé pour retourner la largeur de l'écran
     const { widthScreen } = useScreenWidth();
+    //event de click sur l'icone menu/cross
+    const handleClick = (e) => {
+        setImage(image === menu ? cross : menu);
+        e.target.nextSibling.style.display === 'none' ?
+            e.target.nextSibling.style.display = 'block' :
+            e.target.nextSibling.style.display = 'none';
 
+    }
+
+    // A faire/revoir: composant propre à la liste ? avec condition d'affichage directement dans le header.
     if (widthScreen <= 760) {
         return <>
-            <img src={menu} alt="icone de menu" onClick={event} className="menu-list" />
+            <img src={image} alt="icone de menu" onClick={handleClick} className="menu-list" />
             <nav style={{ display: "none" }}>
                 <ul className="nav-style">
                     <li>Accueil</li>
@@ -35,18 +49,12 @@ function Nav({ event }) {
 
 export default function Header() {
 
-    const handleClick = (e) => {
 
-        e.target.nextSibling.style.display === 'none' ?
-            e.target.nextSibling.style.display = 'block' :
-            e.target.nextSibling.style.display = 'none';
-
-    }
 
 
     return <header>
         <img src={logo} alt="Logo du zoo" className="logo" />
-        <Nav event={handleClick} />
+        <Nav />
 
     </header>
 }
