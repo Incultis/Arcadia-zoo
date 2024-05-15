4e61c58367a820fc5e3e637486053315ca5ca667
inc <p.perez34000@laposte.net>
2024-05-15 09:15:08 +0200

    structure de la page connexion

--- src/components/pages/Connexion.js
+++ src/components/pages/Connexion.js
@@ -1,4 +1,22 @@
+import Input from "../shared/Input";
+import Section from "../shared/Section";
+import styles from "../pages/styles/Form.module.css"
+import HeadContent from "../shared/HeadContent";
+
 export default function Connexion() {
 
-    return <h1>Page de connexion</h1>
+    return <main>
+        <Section background>
+            <HeadContent>
+                <h2>Connexion :</h2>
+            </HeadContent>
+            <form className={styles.containerForm}  >
+                <div className={styles.container}>
+                    <Input label="Email" type="email" name="email" />
+                    <Input label="Mot de passe" type="password" name="password" />
+                    <button className={styles.btnSubmit}>Se connecter</button>
+                </div>
+            </form>
+        </Section>
+    </main>
 }
